f4bd67a033a539e7c033592f0daacf12cf5db857
k.mikov <1konstantinmikov@gmail.com>
2023-05-29 17:12:45 +0300

    29.05

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,24 @@ import { AddButton } from "components/AddButton/AddButton";
 import MainStore from "MainStore";
 import { Players } from "components/Players/Players";
 import { observer } from "mobx-react";
+import { useEffect } from "react";
 
 export const App = observer(() => {
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--window-inner-height",
+      `${window.innerHeight}px`
+    );
+  }, []);
+  
   return (
     <>
       {MainStore.playersStore.players.length > 0 && <Players />}
 
       <AddButton
-        style={ MainStore.playersStore.players.length > 0 ? {} : { marginTop: "auto" } }
+        style={
+          MainStore.playersStore.players.length > 0 ? {} : { marginTop: "auto" }
+        }
         onClick={MainStore.playersStore.addPlayer}
       />
     </>
